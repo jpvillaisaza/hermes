799e7cd9cf69a75c6c4b00fec61b3422e17ab580
Juan Pedro Villa Isaza <584947+jpvillaisaza@users.noreply.github.com>
2025-05-09 19:23:46 -0500

    Add feed discovery

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,57 @@
-console.log("Hermes");
+#!/usr/bin/env node
+
+import * as cheerio from "cheerio";
+
+type Feed = {
+  href: string,
+  title?: string,
+  type: string,
+};
+
+const feedTypes = [
+  "application/rss+xml",
+  "application/atom+xml",
+  "application/feed+json",
+  "application/json",
+];
+
+const extractFeeds = (url: string, html: string): Feed[] => {
+  const $ = cheerio.load(html);
+  return $('link[rel="alternate"]')
+    .get()
+    .flatMap((el) => {
+      const href = $(el).attr("href");
+      const title = $(el).attr("title") || undefined;
+      const type = $(el).attr("type");
+      if (href && type && feedTypes.includes(type)) {
+        return [{
+          href: new URL(href, url).toString(),
+          title,
+          type,
+        }]
+      }
+      return [];
+    })
+    ;
+}
+
+const main = async () => {
+  const url = process.argv[2];
+  if (!url) {
+    console.error('Usage: hermes URL');
+    process.exit(1);
+  }
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  const feeds = extractFeeds(url, await response.text());
+  feeds.forEach(feed => {
+    console.log(`- ${feed.href} (${feed.type})`);
+  });
+}
+
+main().catch((error) => {
+  console.error("Error:", error.message);
+  process.exit(1);
+});
